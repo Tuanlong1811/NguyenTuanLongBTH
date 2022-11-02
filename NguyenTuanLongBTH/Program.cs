3741f3e19fd66d74aeb2063462d2093b50464256
tuanlong1811 <tuanlong181199@gmail.com>
2022-11-02 13:42:57 +0700

    NTL_2610

--- NguyenTuanLongBTH/Program.cs
+++ NguyenTuanLongBTH/Program.cs
@@ -1,4 +1,11 @@
+using NguyenTuanLongBTH_02.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+      options.UseSqlite(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
